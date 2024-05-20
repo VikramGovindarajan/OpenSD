734586d9a1f537eb1dea8b1bdb0d4a97704053f8
vikramg <vickymailbuddy@gmail.com>
2024-05-20 08:01:20 +0530

    start adding functions to read xml input files in cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,13 @@
 int main()
 {
     std::cout << CoolProp::PropsSI("T","P",101325,"Q",0,"Water") << std::endl;
-    return 1;
+    read_separate_xml_files();
+	return 1;
+}
+
+void read_separate_xml_files()
+{
+  // read_settings_xml();
+  // read_model_xml();
+
 }
